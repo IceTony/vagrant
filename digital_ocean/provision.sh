3ca0f13a7382f8e5b676f56ea0c6427c89257651
Aleksei Gogolev <icetony@mail.ru>
2019-09-29 20:21:47 +0300

    Initial commit

---
+++ digital_ocean/provision.sh
@@ -0,0 +1,13 @@
+apt update -y && apt install curl -y &&
+curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $1" -d '{"size_gigabytes":100, "name": "icetony-volume", "description": "Block store for examples", "region": "nyc1"}' "https://api.digitalocean.com/v2/volumes" &&
+droplet_id=$(curl http://169.254.169.254/metadata/v1/id) &&
+curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $1" -d '{"type": "attach", "volume_name": "icetony-volume", "region": "nyc1", "droplet_id": "'"$droplet_id"'" }' "https://api.digitalocean.com/v2/volumes/actions" &&
+echo - - - > /sys/class/scsi_host/host*/scan || true &&
+sleep 5
+/sbin/parted /dev/sda mklabel gpt --script &&
+/sbin/parted /dev/sda mkpart primary 0% 100% --script &&
+sleep 5
+mkfs.ext4 /dev/sda1 &&
+mkdir -p /srv/data &&
+echo "/dev/sda1 /srv/data ext4 auto 0 2" >> /etc/fstab &&
+mount -a
